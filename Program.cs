314245c6ca334a2c2365b15534584474c3d7e27f
Mateus Melo <mateusdevmelo@gmail.com>
2023-12-09 20:18:14 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa("Mateus", "Melo");
+Pessoa p2 = new Pessoa("Kamila", "Melo");
+Pessoa p3 = new Pessoa("Yasmin", "Melo");
+
+
+Curso cursoDeCsharp = new Curso();
+cursoDeCsharp.Nome = "Csharp";
+cursoDeCsharp.Alunos = new List<Pessoa>();
+
+cursoDeCsharp.AdicionarAluno(p1);
+cursoDeCsharp.AdicionarAluno(p2);
+cursoDeCsharp.AdicionarAluno(p3);
+cursoDeCsharp.ListarAlunos();
